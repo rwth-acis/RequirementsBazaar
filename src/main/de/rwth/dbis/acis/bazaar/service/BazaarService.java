2201fecf5aa90a81fe422d828b7d82c42abdacf2
istvank <IK>
2014-06-10 18:24:09 +0200

    Added all REST methods for projects
    GET, POST /projects and GET, PUT, DELETE for /projects/{projectId}.

--- src/main/de/rwth/dbis/acis/bazaar/service/BazaarService.java
+++ src/main/de/rwth/dbis/acis/bazaar/service/BazaarService.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) 2014, RWTH Aachen University.
  * For a list of contributors see the AUTHORS file at the top-level directory
  * of this distribution.
@@ -20,11 +20,18 @@ package de.rwth.dbis.acis.bazaar.service;
 
 import i5.las2peer.api.Service;
 import i5.las2peer.restMapper.RESTMapper;
+import i5.las2peer.restMapper.annotations.Consumes;
+import i5.las2peer.restMapper.annotations.ContentParam;
+import i5.las2peer.restMapper.annotations.DELETE;
 import i5.las2peer.restMapper.annotations.GET;
+import i5.las2peer.restMapper.annotations.POST;
+import i5.las2peer.restMapper.annotations.PUT;
 import i5.las2peer.restMapper.annotations.Path;
 import i5.las2peer.restMapper.annotations.PathParam;
+import i5.las2peer.restMapper.annotations.Produces;
+import i5.las2peer.restMapper.annotations.QueryParam;
 import i5.las2peer.restMapper.annotations.Version;
-
+import i5.las2peer.security.UserAgent;
 
 /**
  * 
@@ -33,59 +40,177 @@ import i5.las2peer.restMapper.annotations.Version;
  * This is the main service class of the Requirements Bazaar
  * 
  * @author István Koren
- *
+ * 
  */
 @Path("bazaar")
 @Version("0.1")
 public class BazaarService extends Service {
-	
-	
+
 	/**
 	 * This method is needed for every RESTful application in LAS2peer.
 	 * 
 	 * @return the mapping to the REST interface.
 	 */
-    public String getRESTMapping() {
-        String result="";
-        try {
-            result= RESTMapper.getMethodsAsXML(this.getClass());
-        } catch (Exception e) {
+	public String getRESTMapping() {
+		String result = "";
+		try {
+			result = RESTMapper.getMethodsAsXML(this.getClass());
+		} catch (Exception e) {
 
-            e.printStackTrace();
-        }
-        return result;
-    }
-    
-    
-    /**********************************
-     * PROJECTS
-     **********************************/
-    
-    /**
-     * This method returns the list of projects on the server. 
-     * 
-     * @return a list of projects.
-     */
-    @GET
-    @Path("projects")
-    public String getProjects() {
-    	return "[]";
-    }
-    
-    
-    /**********************************
-     * REQUIREMENTS
-     **********************************/
-    
-    /**
-     * This method returns the list of requirements for a specific project.
-     * 
-     * @param projectId the ID of the project to retrieve requirements for.
-     * @return a list of requirements
-     */
-    @GET
-    @Path("projects/{projectId}/requirements")
-    public String getRequirements(@PathParam("projectId") String projectId) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	/**********************************
+	 * PROJECTS
+	 **********************************/
+
+	/**
+	 * This method returns the list of projects on the server.
+	 * 
+	 * @return a list of projects.
+	 */
+	@GET
+	@Path("projects")
+	public String getProjects(
+			@QueryParam(name = "page", defaultValue = "1") int page,
+			@QueryParam(name = "per_page", defaultValue = "10") int perPage) {
+		// TODO: if the user is not logged in, return all the public projects.
+		// Otherwise return all the user can see.
+		return "[]";
+	}
+
+	/**
+	 * This method allows to create a new project.
+	 * 
+	 * @param String
+	 *            project a JSON string containing all the parameters.
+	 * @return
+	 */
+	@POST
+	@Path("projects")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String createProject(@ContentParam String project) {
+		long userId = ((UserAgent) getActiveAgent()).getId();
+		// TODO: check whether the current user may create a new project
+		// TODO: check whether all required parameters are entered
+
+		return ("{success=false}");
+	}
+
+	/**
+	 * This method allows to retrieve a certain project.
+	 * 
+	 * @param projectId
+	 *            the id of the project to retrieve
+	 * @return the details of a certain project.
+	 */
+	@GET
+	@Path("projects/{projectId}")
+	@Produces("application/json")
+	public String getProject(@PathParam("projectId") int projectId) {
+		// TODO: check whether the current user may request this project
+
+		return "{}";
+	}
+
+	/**
+	 * Allows to update a certain project.
+	 * 
+	 * @param projectId
+	 *            the id of the project to update.
+	 * @return a JSON string containing whether the operation was successful or
+	 *         not.
+	 */
+	@PUT
+	@Path("projects/{projectId}")
+	@Produces("application/json")
+	public String updateProject(@PathParam("projectId") int projectId) {
+		// TODO: check if user can change this project
+		return "{success=false}";
+	}
+
+	@DELETE
+	@Path("projects/{projectId}")
+	@Produces("application/json")
+	public String deleteProject(@PathParam("projectId") int projectId) {
+		// TODO: check if user can delete this project
+		return "{success=false}";
+	}
+
+	/**********************************
+	 * REQUIREMENTS
+	 **********************************/
+
+	/**
+	 * This method returns the list of requirements for a specific project.
+	 * 
+	 * @param projectId
+	 *            the ID of the project to retrieve requirements for.
+	 * @return a list of requirements
+	 */
+	@GET
+	@Path("projects/{projectId}/requirements")
+	public String getRequirements(@PathParam("projectId") int projectId,
+			@QueryParam(name = "page", defaultValue = "1") int page,
+			@QueryParam(name = "per_page", defaultValue = "10") int perPage) {
 		return "The requirements for project " + projectId + ".";
+	}
+
+	/**
+	 * This method allows to create a new requirement.
+	 * 
+	 * @param projectId
+	 *            the ID of the project to create the requirement in.
+	 * @param String
+	 *            requirement a JSON string containing all the parameters.
+	 * @return
+	 */
+	@POST
+	@Path("projects/{projectId}/requirements")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public String createRequirement(@PathParam("projectId") int projectId,
+			@ContentParam String requirement) {
+		long userId = ((UserAgent) getActiveAgent()).getId();
+		// TODO: check whether the current user may create a new requirement
+		// TODO: check whether all required parameters are entered
+
+		return ("{success=false}");
+	}
+
+	/**
+	 * This method returns a specific requirement within a project.
+	 * 
+	 * @param projectId
+	 *            the ID of the project to retrieve requirements for.
+	 * @param requirementId
+	 *            the ID of the requirement to retrieve.
+	 * @return a list of requirements
+	 */
+	@GET
+	@Path("projects/{projectId}/requirements/{requirementId}")
+	public String getRequirements(@PathParam("projectId") int projectId,
+			@PathParam("requirementId") int requirementId) {
+		return "[]";
+	}
+
+	/**********************************
+	 * USERS
+	 **********************************/
+
+	/**
+	 * Retrieves a list of all users.
+	 * 
+	 * @return a JSON encoded list of all users.
+	 */
+	@GET
+	@Path("users")
+	@Produces("application/json")
+	public String getUsers() {
+		// TODO: check if the admin user wants to retrieve all users.
+		return "[]";
 	}
 }
