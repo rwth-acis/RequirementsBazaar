bb76f65411fc54110b4fafa50c416583aaaebfe6
Martin <martin.hug@rwth-aachen.de>
2015-05-30 14:37:57 +0200

    Format all sql files to code format

--- etc/ReqBaz_create.sql
+++ etc/ReqBaz_create.sql
@@ -4,183 +4,195 @@
 -- Generated at Thu Mar 05 20:06:24 UTC 2015
 
 
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- tables
 -- Table attachments
 CREATE TABLE IF NOT EXISTS attachments (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    creation_time timestamp    NOT NULL ,
-    Requirement_Id int    NOT NULL ,
-    User_Id int    NOT NULL ,
-    title varchar(255)    NOT NULL ,
-    discriminator char(1)    NOT NULL ,
-    file_path varchar(500)    NULL ,
-    description text    NULL ,
-    story text    NULL ,
-    subject varchar(255)    NULL ,
-    object varchar(255)    NULL ,
-    object_desc varchar(255)    NULL ,
-    CONSTRAINT attachments_pk PRIMARY KEY (Id),
-	CONSTRAINT Attachement_Requirement FOREIGN KEY Attachement_Requirement (Requirement_Id) REFERENCES requirements (Id) ON DELETE CASCADE,
-	CONSTRAINT Attachement_User FOREIGN KEY Attachement_User (User_Id) REFERENCES users (Id)
+  Id             INT          NOT NULL  AUTO_INCREMENT,
+  creation_time  TIMESTAMP    NOT NULL,
+  Requirement_Id INT          NOT NULL,
+  User_Id        INT          NOT NULL,
+  title          VARCHAR(255) NOT NULL,
+  discriminator  CHAR(1)      NOT NULL,
+  file_path      VARCHAR(500) NULL,
+  description    TEXT         NULL,
+  story          TEXT         NULL,
+  subject        VARCHAR(255) NULL,
+  object         VARCHAR(255) NULL,
+  object_desc    VARCHAR(255) NULL,
+  CONSTRAINT attachments_pk PRIMARY KEY (Id),
+  CONSTRAINT Attachement_Requirement FOREIGN KEY Attachement_Requirement (Requirement_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Attachement_User FOREIGN KEY Attachement_User (User_Id) REFERENCES users (Id)
 );
 
 -- Table comments
 CREATE TABLE IF NOT EXISTS comments (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    message text    NOT NULL ,
-    creation_time timestamp    NOT NULL ,
-    Requirement_Id int    NOT NULL ,
-    User_Id int    NOT NULL ,
-    CONSTRAINT comments_pk PRIMARY KEY (Id),
-	CONSTRAINT Comment_Requirement FOREIGN KEY Comment_Requirement (Requirement_Id) REFERENCES requirements (Id) ON DELETE CASCADE,
-	CONSTRAINT Comment_User FOREIGN KEY Comment_User (User_Id)  REFERENCES users (Id)
+  Id             INT       NOT NULL  AUTO_INCREMENT,
+  message        TEXT      NOT NULL,
+  creation_time  TIMESTAMP NOT NULL,
+  Requirement_Id INT       NOT NULL,
+  User_Id        INT       NOT NULL,
+  CONSTRAINT comments_pk PRIMARY KEY (Id),
+  CONSTRAINT Comment_Requirement FOREIGN KEY Comment_Requirement (Requirement_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Comment_User FOREIGN KEY Comment_User (User_Id) REFERENCES users (Id)
 );
 
 -- Table components
 CREATE TABLE IF NOT EXISTS components (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    name varchar(255)    NOT NULL ,
-    description text    NOT NULL ,
-    Project_Id int    NOT NULL ,
-    Leader_Id int    NOT NULL ,
-    CONSTRAINT components_pk PRIMARY KEY (Id),
-	CONSTRAINT Component_Project FOREIGN KEY Component_Project (Project_Id) REFERENCES projects (Id),
-	CONSTRAINT Components_Users FOREIGN KEY Components_Users (Leader_Id) REFERENCES users (Id)
+  Id          INT          NOT NULL  AUTO_INCREMENT,
+  name        VARCHAR(255) NOT NULL,
+  description TEXT         NOT NULL,
+  Project_Id  INT          NOT NULL,
+  Leader_Id   INT          NOT NULL,
+  CONSTRAINT components_pk PRIMARY KEY (Id),
+  CONSTRAINT Component_Project FOREIGN KEY Component_Project (Project_Id) REFERENCES projects (Id),
+  CONSTRAINT Components_Users FOREIGN KEY Components_Users (Leader_Id) REFERENCES users (Id)
 );
 
 -- Table developers
 CREATE TABLE IF NOT EXISTS developers (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Requirement_Id int    NOT NULL ,
-    User_Id int    NOT NULL ,
-    creation_time timestamp    NOT NULL ,
-    CONSTRAINT developers_pk PRIMARY KEY (Id),
-	CONSTRAINT Developer_Requirement FOREIGN KEY Developer_Requirement (Requirement_Id)  REFERENCES requirements (Id)  ON DELETE CASCADE,
-	CONSTRAINT Developer_User FOREIGN KEY Developer_User (User_Id)  REFERENCES users (Id)
+  Id             INT       NOT NULL  AUTO_INCREMENT,
+  Requirement_Id INT       NOT NULL,
+  User_Id        INT       NOT NULL,
+  creation_time  TIMESTAMP NOT NULL,
+  CONSTRAINT developers_pk PRIMARY KEY (Id),
+  CONSTRAINT Developer_Requirement FOREIGN KEY Developer_Requirement (Requirement_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Developer_User FOREIGN KEY Developer_User (User_Id) REFERENCES users (Id)
 );
 
 -- Table followers
 CREATE TABLE IF NOT EXISTS followers (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Requirement_Id int    NOT NULL ,
-    User_Id int    NOT NULL ,
-    creation_time timestamp    NULL ,
-    CONSTRAINT followers_pk PRIMARY KEY (Id),
-	CONSTRAINT Follower_Requirement FOREIGN KEY Follower_Requirement (Requirement_Id)  REFERENCES requirements (Id)  ON DELETE CASCADE,
-	CONSTRAINT Follower_User FOREIGN KEY Follower_User (User_Id)  REFERENCES users (Id)
+  Id             INT       NOT NULL  AUTO_INCREMENT,
+  Requirement_Id INT       NOT NULL,
+  User_Id        INT       NOT NULL,
+  creation_time  TIMESTAMP NULL,
+  CONSTRAINT followers_pk PRIMARY KEY (Id),
+  CONSTRAINT Follower_Requirement FOREIGN KEY Follower_Requirement (Requirement_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Follower_User FOREIGN KEY Follower_User (User_Id) REFERENCES users (Id)
 );
 
 -- Table privileges
 CREATE TABLE IF NOT EXISTS privileges (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    name varchar(100)    NOT NULL ,
-    CONSTRAINT privileges_pk PRIMARY KEY (Id)
+  Id   INT          NOT NULL  AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  CONSTRAINT privileges_pk PRIMARY KEY (Id)
 );
 
 -- Table projects
 CREATE TABLE IF NOT EXISTS projects (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    name varchar(255)    NOT NULL ,
-    description text    NOT NULL ,
-    visibility char(1)    NOT NULL ,
-    Leader_Id int    NOT NULL ,
-    Default_Components_Id int    NULL ,
-    CONSTRAINT projects_pk PRIMARY KEY (Id),
-	CONSTRAINT Projects_Components FOREIGN KEY Projects_Components (Default_Components_Id)  REFERENCES components (Id),
-	CONSTRAINT Projects_Users FOREIGN KEY Projects_Users (Leader_Id)  REFERENCES users (Id)
+  Id                    INT          NOT NULL  AUTO_INCREMENT,
+  name                  VARCHAR(255) NOT NULL,
+  description           TEXT         NOT NULL,
+  visibility            CHAR(1)      NOT NULL,
+  Leader_Id             INT          NOT NULL,
+  Default_Components_Id INT          NULL,
+  CONSTRAINT projects_pk PRIMARY KEY (Id),
+  CONSTRAINT Projects_Components FOREIGN KEY Projects_Components (Default_Components_Id) REFERENCES components (Id),
+  CONSTRAINT Projects_Users FOREIGN KEY Projects_Users (Leader_Id) REFERENCES users (Id)
 );
 
 -- Table requirements
 CREATE TABLE IF NOT EXISTS requirements (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    title varchar(255)    NOT NULL ,
-    description text    NULL ,
-    creation_time timestamp    NOT NULL ,
-    Lead_developer_Id int    NOT NULL ,
-    Creator_Id int    NOT NULL ,
-    Project_Id int    NOT NULL ,
-    CONSTRAINT requirements_pk PRIMARY KEY (Id),
-	CONSTRAINT Creator FOREIGN KEY Creator (Creator_Id)  REFERENCES users (Id),
-	CONSTRAINT LeadDeveloper FOREIGN KEY LeadDeveloper (Lead_developer_Id)  REFERENCES users (Id),
-	CONSTRAINT Requirement_Project FOREIGN KEY Requirement_Project (Project_Id)   REFERENCES projects (Id)
+  Id                INT          NOT NULL  AUTO_INCREMENT,
+  title             VARCHAR(255) NOT NULL,
+  description       TEXT         NULL,
+  creation_time     TIMESTAMP    NOT NULL,
+  Lead_developer_Id INT          NOT NULL,
+  Creator_Id        INT          NOT NULL,
+  Project_Id        INT          NOT NULL,
+  CONSTRAINT requirements_pk PRIMARY KEY (Id),
+  CONSTRAINT Creator FOREIGN KEY Creator (Creator_Id) REFERENCES users (Id),
+  CONSTRAINT LeadDeveloper FOREIGN KEY LeadDeveloper (Lead_developer_Id) REFERENCES users (Id),
+  CONSTRAINT Requirement_Project FOREIGN KEY Requirement_Project (Project_Id) REFERENCES projects (Id)
 );
 
 -- Table role_privilege
 CREATE TABLE IF NOT EXISTS role_privilege (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Roles_Id int    NOT NULL ,
-    Privileges_Id int    NOT NULL ,
-    CONSTRAINT role_privilege_pk PRIMARY KEY (Id),
-	CONSTRAINT Role_Privilege_Privileges FOREIGN KEY Role_Privilege_Privileges (Privileges_Id)  REFERENCES privileges (Id)  ON DELETE CASCADE,
-	CONSTRAINT Role_Privilege_Roles FOREIGN KEY Role_Privilege_Roles (Roles_Id)  REFERENCES roles (Id)  ON DELETE CASCADE
+  Id            INT NOT NULL  AUTO_INCREMENT,
+  Roles_Id      INT NOT NULL,
+  Privileges_Id INT NOT NULL,
+  CONSTRAINT role_privilege_pk PRIMARY KEY (Id),
+  CONSTRAINT Role_Privilege_Privileges FOREIGN KEY Role_Privilege_Privileges (Privileges_Id) REFERENCES privileges (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Role_Privilege_Roles FOREIGN KEY Role_Privilege_Roles (Roles_Id) REFERENCES roles (Id)
+    ON DELETE CASCADE
 );
 
 -- Table role_role
 CREATE TABLE IF NOT EXISTS role_role (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Child_Id int    NOT NULL ,
-    Parent_Id int    NOT NULL ,
-    CONSTRAINT role_role_pk PRIMARY KEY (Id),
-	CONSTRAINT Role_Child FOREIGN KEY Role_Child (Child_Id)   REFERENCES roles (Id)  ON DELETE CASCADE,
-	CONSTRAINT Role_Parent FOREIGN KEY Role_Parent (Parent_Id)  REFERENCES roles (Id)   ON DELETE CASCADE
+  Id        INT NOT NULL  AUTO_INCREMENT,
+  Child_Id  INT NOT NULL,
+  Parent_Id INT NOT NULL,
+  CONSTRAINT role_role_pk PRIMARY KEY (Id),
+  CONSTRAINT Role_Child FOREIGN KEY Role_Child (Child_Id) REFERENCES roles (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Role_Parent FOREIGN KEY Role_Parent (Parent_Id) REFERENCES roles (Id)
+    ON DELETE CASCADE
 );
 
 -- Table roles
 CREATE TABLE IF NOT EXISTS roles (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    name varchar(50)    NULL ,
-    CONSTRAINT roles_pk PRIMARY KEY (Id),
-	UNIQUE KEY Role_idx_1 (name)
+  Id   INT         NOT NULL  AUTO_INCREMENT,
+  name VARCHAR(50) NULL,
+  CONSTRAINT roles_pk PRIMARY KEY (Id),
+  UNIQUE KEY Role_idx_1 (name)
 );
 
 
 -- Table tags
 CREATE TABLE IF NOT EXISTS tags (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Components_Id int    NOT NULL ,
-    Requirements_Id int    NOT NULL ,
-    CONSTRAINT tags_pk PRIMARY KEY (Id),
-	CONSTRAINT Tags_Components FOREIGN KEY Tags_Components (Components_Id)    REFERENCES components (Id),
-	CONSTRAINT Tags_Requirements FOREIGN KEY Tags_Requirements (Requirements_Id)    REFERENCES requirements (Id)    ON DELETE CASCADE
+  Id              INT NOT NULL  AUTO_INCREMENT,
+  Components_Id   INT NOT NULL,
+  Requirements_Id INT NOT NULL,
+  CONSTRAINT tags_pk PRIMARY KEY (Id),
+  CONSTRAINT Tags_Components FOREIGN KEY Tags_Components (Components_Id) REFERENCES components (Id),
+  CONSTRAINT Tags_Requirements FOREIGN KEY Tags_Requirements (Requirements_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE
 );
 
 -- Table user_role
 CREATE TABLE IF NOT EXISTS user_role (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    Roles_Id int    NOT NULL ,
-    Users_Id int    NOT NULL ,
-    context_info varchar(255)    NULL ,
-    CONSTRAINT user_role_pk PRIMARY KEY (Id),
-	CONSTRAINT User_Role_Roles FOREIGN KEY User_Role_Roles (Roles_Id)    REFERENCES roles (Id)    ON DELETE CASCADE,
-	CONSTRAINT User_Role_Users FOREIGN KEY User_Role_Users (Users_Id)    REFERENCES users (Id)    ON DELETE CASCADE
+  Id           INT          NOT NULL  AUTO_INCREMENT,
+  Roles_Id     INT          NOT NULL,
+  Users_Id     INT          NOT NULL,
+  context_info VARCHAR(255) NULL,
+  CONSTRAINT user_role_pk PRIMARY KEY (Id),
+  CONSTRAINT User_Role_Roles FOREIGN KEY User_Role_Roles (Roles_Id) REFERENCES roles (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT User_Role_Users FOREIGN KEY User_Role_Users (Users_Id) REFERENCES users (Id)
+    ON DELETE CASCADE
 );
 
 -- Table users
 CREATE TABLE IF NOT EXISTS users (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    first_name varchar(150)    NULL ,
-    last_name varchar(150)    NULL ,
-    email varchar(255)    NOT NULL ,
-    admin BOOLEAN    NOT NULL ,
-    Las2peer_Id bigint    NOT NULL ,
-    user_name varchar(255)    NULL ,
-    profile_image text    NULL ,
-    CONSTRAINT users_pk PRIMARY KEY (Id)
+  Id            INT          NOT NULL  AUTO_INCREMENT,
+  first_name    VARCHAR(150) NULL,
+  last_name     VARCHAR(150) NULL,
+  email         VARCHAR(255) NOT NULL,
+  admin         BOOLEAN      NOT NULL,
+  Las2peer_Id   BIGINT       NOT NULL,
+  user_name     VARCHAR(255) NULL,
+  profile_image TEXT         NULL,
+  CONSTRAINT users_pk PRIMARY KEY (Id)
 );
 
 -- Table votes
 CREATE TABLE IF NOT EXISTS votes (
-    Id int    NOT NULL  AUTO_INCREMENT,
-    is_upvote BOOLEAN    NOT NULL ,
-    Requirement_Id int    NOT NULL ,
-    User_Id int    NOT NULL ,
-    creation_time timestamp    NOT NULL ,
-    CONSTRAINT votes_pk PRIMARY KEY (Id),
-	CONSTRAINT Votes_Requirement FOREIGN KEY Votes_Requirement (Requirement_Id)    REFERENCES requirements (Id)    ON DELETE CASCADE,
-	CONSTRAINT Votes_User FOREIGN KEY Votes_User (User_Id)    REFERENCES users (Id)
+  Id             INT       NOT NULL  AUTO_INCREMENT,
+  is_upvote      BOOLEAN   NOT NULL,
+  Requirement_Id INT       NOT NULL,
+  User_Id        INT       NOT NULL,
+  creation_time  TIMESTAMP NOT NULL,
+  CONSTRAINT votes_pk PRIMARY KEY (Id),
+  CONSTRAINT Votes_Requirement FOREIGN KEY Votes_Requirement (Requirement_Id) REFERENCES requirements (Id)
+    ON DELETE CASCADE,
+  CONSTRAINT Votes_User FOREIGN KEY Votes_User (User_Id) REFERENCES users (Id)
 );
 
 
-SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS = 1;
--- etc/ReqBaz_demo_data.sql
+++ etc/ReqBaz_demo_data.sql
@@ -1,106 +1,110 @@
-SET FOREIGN_KEY_CHECKS=0;
+SET FOREIGN_KEY_CHECKS = 0;
 
-REPLACE INTO `reqbaz`.`users` 
-(`Id`,`first_name`, `last_name`, `email`, `admin`, `Las2peer_Id`, `user_name`, `profile_image`)
- VALUES 
-('1',NULL, NULL, 'anonymous@requirements-bazaar.org', '0', '-1722613621014065292', 'anonymous','https://api.learning-layers.eu/profile.png');
-
-
-REPLACE INTO `reqbaz`.`projects` 
-(`Id`,`name`, `description`, `visibility`, `Leader_Id`, `Default_Components_Id`) 
+REPLACE INTO `reqbaz`.`users`
+(`Id`, `first_name`, `last_name`, `email`, `admin`, `Las2peer_Id`, `user_name`, `profile_image`)
 VALUES
-('1','Layers', 'This is everything about Layers', '+', '1', NULL),
-('2','Requirements Bazaar', 'This project is about Requirement Bazaar', '+', '1',NULL);
+  ('1', NULL, NULL, 'anonymous@requirements-bazaar.org', '0', '-1722613621014065292', 'anonymous',
+   'https://api.learning-layers.eu/profile.png');
 
 
+REPLACE INTO `reqbaz`.`projects`
+(`Id`, `name`, `description`, `visibility`, `Leader_Id`, `Default_Components_Id`)
+VALUES
+  ('1', 'Layers', 'This is everything about Layers', '+', '1', NULL),
+  ('2', 'Requirements Bazaar', 'This project is about Requirement Bazaar', '+', '1', NULL);
 
-REPLACE INTO `reqbaz`.`components` 
-(`Id`,`name`, `description`, `Project_Id`, `Leader_Id`) 
-VALUES 
-('1','Layers Box', 'This is a play box component for the Layers project.', '1 ', '1'),
-('2','Web App', 'Post your requirements about the Requirement Bazaar Web app under this component', '2', '1');
 
-UPDATE `reqbaz`.`projects` SET `Default_Components_Id`='1' WHERE `Id`='1 '; 
-UPDATE `reqbaz`.`projects` SET `Default_Components_Id`='2' WHERE `Id`='2 '; 
+REPLACE INTO `reqbaz`.`components`
+(`Id`, `name`, `description`, `Project_Id`, `Leader_Id`)
+VALUES
+  ('1', 'Layers Box', 'This is a play box component for the Layers project.', '1 ', '1'),
+  ('2', 'Web App', 'Post your requirements about the Requirement Bazaar Web app under this component', '2', '1');
+
+UPDATE `reqbaz`.`projects`
+SET `Default_Components_Id` = '1'
+WHERE `Id` = '1 ';
+UPDATE `reqbaz`.`projects`
+SET `Default_Components_Id` = '2'
+WHERE `Id` = '2 ';
 
 
 REPLACE INTO `roles` (`Id`, `name`) VALUES
-(1, 'Anonymous'),
-(2, 'LoggedInUser'),
-(3, 'ProjectAdmin'),
-(4, 'SystemAdmin');
+  (1, 'Anonymous'),
+  (2, 'LoggedInUser'),
+  (3, 'ProjectAdmin'),
+  (4, 'SystemAdmin');
 
 REPLACE INTO `privileges` (`Id`, `name`) VALUES
-(1, 'Create_PROJECT'),
-(2, 'Read_PROJECT'),
-(3, 'Read_PUBLIC_PROJECT'),
-(4, 'Modify_PROJECT'),
-(5, 'Create_COMPONENT'),
-(6, 'Read_COMPONENT'),
-(7, 'Read_PUBLIC_COMPONENT'),
-(8, 'Modify_COMPONENT'),
-(9, 'Create_REQUIREMENT'),
-(10, 'Read_REQUIREMENT'),
-(11, 'Read_PUBLIC_REQUIREMENT'),
-(12, 'Modify_REQUIREMENT'),
-(13, 'Create_COMMENT'),
-(14, 'Read_COMMENT'),
-(15, 'Read_PUBLIC_COMMENT'),
-(16, 'Modify_COMMENT'),
-(17, 'Create_ATTACHMENT'),
-(18, 'Read_ATTACHMENT'),
-(19, 'Read_PUBLIC_ATTACHMENT'),
-(20, 'Modify_ATTACHMENT'),
-(21, 'Create_VOTE'),
-(22, 'Delete_VOTE'),
-(23, 'Create_FOLLOW'),
-(24, 'Delete_FOLLOW'),
-(25, 'Create_DEVELOP'),
-(26, 'Delete_DEVELOP');
-	
+  (1, 'Create_PROJECT'),
+  (2, 'Read_PROJECT'),
+  (3, 'Read_PUBLIC_PROJECT'),
+  (4, 'Modify_PROJECT'),
+  (5, 'Create_COMPONENT'),
+  (6, 'Read_COMPONENT'),
+  (7, 'Read_PUBLIC_COMPONENT'),
+  (8, 'Modify_COMPONENT'),
+  (9, 'Create_REQUIREMENT'),
+  (10, 'Read_REQUIREMENT'),
+  (11, 'Read_PUBLIC_REQUIREMENT'),
+  (12, 'Modify_REQUIREMENT'),
+  (13, 'Create_COMMENT'),
+  (14, 'Read_COMMENT'),
+  (15, 'Read_PUBLIC_COMMENT'),
+  (16, 'Modify_COMMENT'),
+  (17, 'Create_ATTACHMENT'),
+  (18, 'Read_ATTACHMENT'),
+  (19, 'Read_PUBLIC_ATTACHMENT'),
+  (20, 'Modify_ATTACHMENT'),
+  (21, 'Create_VOTE'),
+  (22, 'Delete_VOTE'),
+  (23, 'Create_FOLLOW'),
+  (24, 'Delete_FOLLOW'),
+  (25, 'Create_DEVELOP'),
+  (26, 'Delete_DEVELOP');
+
 
 REPLACE INTO `role_privilege` (`Id`, `Roles_Id`, `Privileges_Id`) VALUES
-(1, 1, 3),
-(2, 1, 7),
-(3, 1, 11),
-(4, 1, 15),
-(5, 1, 19),
-(6, 4, 1),
-(7, 4, 2),
-(8, 4, 8),
-(9, 4, 7),
-(10, 4, 6),
-(11, 4, 5),
-(12, 4, 3),
-(13, 4, 4),
-(14, 4, 9),
-(15, 4, 10),
-(16, 4, 11),
-(17, 4, 12),
-(18, 4, 13),
-(19, 4, 14),
-(20, 4, 16),
-(21, 4, 17),
-(22, 4, 18),
-(23, 4, 19),
-(24, 4, 20),
-(25, 4, 21),
-(26, 4, 22),
-(27, 4, 23),
-(28, 4, 24),
-(29, 4, 25),
-(30, 4, 26);
+  (1, 1, 3),
+  (2, 1, 7),
+  (3, 1, 11),
+  (4, 1, 15),
+  (5, 1, 19),
+  (6, 4, 1),
+  (7, 4, 2),
+  (8, 4, 8),
+  (9, 4, 7),
+  (10, 4, 6),
+  (11, 4, 5),
+  (12, 4, 3),
+  (13, 4, 4),
+  (14, 4, 9),
+  (15, 4, 10),
+  (16, 4, 11),
+  (17, 4, 12),
+  (18, 4, 13),
+  (19, 4, 14),
+  (20, 4, 16),
+  (21, 4, 17),
+  (22, 4, 18),
+  (23, 4, 19),
+  (24, 4, 20),
+  (25, 4, 21),
+  (26, 4, 22),
+  (27, 4, 23),
+  (28, 4, 24),
+  (29, 4, 25),
+  (30, 4, 26);
 
 
 REPLACE INTO `role_role` (`Id`, `Child_Id`, `Parent_Id`) VALUES
-(1, 2, 1),
-(2, 3, 2),
-(3, 4, 3);
+  (1, 2, 1),
+  (2, 3, 2),
+  (3, 4, 3);
 
 REPLACE INTO `user_role` (`Id`, `Roles_Id`, `Users_Id`) VALUES
-(1, 1, 1);
+  (1, 1, 1);
 
-SET FOREIGN_KEY_CHECKS=1;
+SET FOREIGN_KEY_CHECKS = 1;
 
 
 
