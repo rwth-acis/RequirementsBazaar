8f78c68b3627e5154d1c48d69a93f655acbf0a2e
Martin <martin.hug@rwth-aachen.de>
2015-07-03 07:16:34 +0200

    Update LAS2peer to new version 0.4 SNAPSHOT #RB-221

--- bin/start_network_debug.bat
+++ bin/start_network_debug.bat
@@ -1,10 +1,11 @@
 :: this script starts a las2peer node providing the example service of this project
 :: pls execute it from the bin folder of your deployment by double-clicking on it
 
+%~d0
+cd %~p0
 cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
 java -cp %CLASSPATH% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5432 i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('de.rwth.dbis.acis.bazaar.service.BazaarService','SampleServicePass') startWebConnector interactive
-
 pause
