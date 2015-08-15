7f17a39130b53d5d27e67a2c6d9fbf20b31a33fe
Martin <martin.hug@rwth-aachen.de>
2015-08-15 22:02:11 +0200

    Added new services to start scripts

--- bin/start_network_debug.bat
+++ bin/start_network_debug.bat
@@ -7,5 +7,5 @@ cd ..
 set BASE=%CD%
 set CLASSPATH="%BASE%/lib/*;"
 
-java -cp %CLASSPATH% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5432 i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('de.rwth.dbis.acis.bazaar.service.BazaarService','SampleServicePass') startWebConnector interactive
+java -cp %CLASSPATH% -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5432 i5.las2peer.tools.L2pNodeLauncher -w -p 9011 uploadStartupDirectory('etc/startup') startService('de.rwth.dbis.acis.bazaar.service.BazaarService','SampleServicePass') startService('de.rwth.dbis.acis.bazaar.service.UsersResource','SampleServicePass') startService('de.rwth.dbis.acis.bazaar.service.ProjectsResource','SampleServicePass') startWebConnector interactive
 pause
