b023c81ecd1e8ed60bb9c6c9838f8e8e6ab2ac19
Martin <martin.hug@rwth-aachen.de>
2016-03-17 21:50:18 +0100

    Update jooq generation script to 3.7.2

--- bin/JOOQGeneration/run.bat
+++ bin/JOOQGeneration/run.bat
@@ -1,1 +1,1 @@
-java -classpath ../../lib/jooq-3.4.2.jar;../../lib/jooq-meta-3.4.2.jar;../../lib/jooq-codegen-3.4.2.jar;../../lib/mysql-connector-java-5.1.33.jar;. org.jooq.util.GenerationTool /reqbaz_generation_info.xml
+java -classpath ../../lib/jooq-3.7.2.jar;../../lib/jooq-meta-3.7.2.jar;../../lib/jooq-codegen-3.7.2.jar;../../lib/mysql-connector-java-5.1.33.jar;. org.jooq.util.GenerationTool /reqbaz_generation_info.xml
