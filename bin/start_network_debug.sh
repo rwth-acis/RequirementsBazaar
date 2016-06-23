0870f56d57e1ba25c70022167a2fe63793a5149d
Martin <martin.hug@rwth-aachen.de>
2016-06-23 13:43:37 +0200

    First commit for upgrading to version 0.5.4. Not complete!

--- bin/start_network_debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this script starts a las2peer node providing the example service of this project
-# pls execute it from the root folder of your deployment, e. g. ./bin/start_network.sh
-
-java -cp "lib/*" -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5432 i5.las2peer.tools.L2pNodeLauncher -p 9011 uploadStartupDirectory\(\'etc/startup\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.BazaarService\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.UsersResource\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.ProjectsResource\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.ComponentsResource\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.RequirementsResource\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.CommentsResource\',\'SampleServicePass\'\) startService\(\'de.rwth.dbis.acis.bazaar.service.AttachmentsResource\',\'SampleServicePass\'\) startWebConnector interactive
