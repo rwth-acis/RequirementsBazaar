1489cc3640d09ccfcbfb78398911f9801a951d11
Martin Hug <martin.hug@rwth-aachen.de>
2017-03-09 10:00:18 +0100

    Update jooq to version 3.9.1

--- bin/JOOQGeneration/run.sh
+++ bin/JOOQGeneration/run.sh
@@ -1,1 +1,1 @@
-java -classpath ../../lib/jooq-3.8.2.jar:../../lib/jooq-meta-3.8.2.jar:../../lib/jooq-codegen-3.8.2.jar:../../lib/mysql-connector-java-5.1.6.jar:. org.jooq.util.GenerationTool /reqbaz_generation_info.xml
+java -classpath ../../lib/jooq-3.9.1.jar:../../lib/jooq-meta-3.9.1.jar:../../lib/jooq-codegen-3.9.1.jar:../../lib/mysql-connector-java-5.1.6.jar:. org.jooq.util.GenerationTool /reqbaz_generation_info.xml
