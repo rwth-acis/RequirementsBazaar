42bdf0383d023a283089c3e00f59b6ef221bc72d
Thore <thore@kruess.xyz>
2021-08-06 17:58:27 +0200

    Fix old migrations included in build export

--- reqbaz/src/main/resources/migrations/V2__user_las2peer_id_to_string.sql
+++
@@ -1,7 +0,0 @@
-SET FOREIGN_KEY_CHECKS = 0;
-
-ALTER TABLE reqbaz.user change las2peer_id las2peer_id VARCHAR(128);
-
-UPDATE reqbaz.user SET las2peer_id = "anonymous" where id = 1;
-
-SET FOREIGN_KEY_CHECKS = 1;
